27f7a6207d9ec18b8fbd79bfdd629679b3f7faf4
aglio0o0oliio <triple_threater@yahoo.co.jp>
2026-09-10 02:58:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,38 @@
-self.addEventListener("install",function(e){e.waitUntil(caches.open("babyface-signage-v1").then(function(c){return c.addAll(["./","./index.html","./manifest.webmanifest"]);}).catch(function(){}));});
-self.addEventListener("fetch",function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));});
+const CACHE = "babyface-koryo-signage-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./ranking.png",
+  "./drink.png",
+  "./instagram.png",
+  "./staff.png",
+  "./food.jpg",
+  "./dessert.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+    )
+  );
+});

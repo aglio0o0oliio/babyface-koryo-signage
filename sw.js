12d9ddf51c76e8ebd54aa172563c9e182c3856b5
aglio0o0oliio <triple_threater@yahoo.co.jp>
2026-09-10 18:17:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "babyface-koryo-signage-v3";
+const CACHE_NAME = "babyface-koryo-signage-v4";
 const ASSETS = [
   "./",
   "./index.html",
@@ -31,8 +31,6 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });

0f7b36e7300ebf252447b11a27cb4a4dd4c7593e
aglio0o0oliio <triple_threater@yahoo.co.jp>
2026-09-11 12:02:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,36 +1,4 @@
-const CACHE_NAME = "babyface-koryo-signage-v4";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./ranking.png",
-  "./payment.png",
-  "./drink.png",
-  "./instagram.mp4",
-  "./food.jpg",
-  "./dessert.jpg"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
-});
+const C="babyface-koryo-final-v1",A=["./","./index.html","./manifest.webmanifest","./payment.jpg","./ranking.png","./instagram.mp4","./food.jpg","./dessert.jpg","./drink.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
